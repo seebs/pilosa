62e93d21f31a74e79969e2a534169ce9d477a0d1
Seebs <seebs@seebs.net>
2019-07-11 14:31:46 -0500

    ReadOnly and Transactional bitmap interfaces

--- data/bitmap.go
+++ data/bitmap.go
@@ -1,7 +1,6 @@
 package data
 
-// Bitmap represents an indexable vector of 1<<64 bits.
-type Bitmap interface {
+type ReadOnlyBitmap interface {
 	// Any reports whether at least one bit is set.
 	Any() bool
 	// AnyRange reports whether at least one bit is set between first and last, inclusive.
@@ -17,37 +16,60 @@ type Bitmap interface {
 	Slice() (values []uint64, overflow bool)
 	// SliceRange is like Slice(), but within a limited range.
 	SliceRange(first, last uint64) (values []uint64, overflow bool)
+	// GetContainer yields a Container holding the 1<<16 values starting at
+	// key * (1<<16). For instance, if key is 1, it returns the bits from
+	// 65,536 to 131,071 inclusive.
+	//
+	// If the bitmap is a ReadOnlyBitmap, do not attempt to modify the
+	// container. The bitmap may not enforce this.
+	GetContainer(key uint64) Container
+	// Union, Difference, Intersection, and Xor create new bitmaps based
+	// on this bitmap and other provided bitmaps.
+	Union(...Bitmap) Bitmap
+	Difference(...Bitmap) Bitmap
+	Intersection(...Bitmap) Bitmap
+	Xor(...Bitmap) Bitmap
+	// OffsetRange yields a new bitmap containing values from first to last,
+	// with all positions increased by offset. All three values must be multiples
+	// of 1<<16. The resulting bitmap should be treated as read-only if the
+	// bitmap it is derived from was read-only.
+	OffsetRange(offset, first, last uint64) Bitmap
+	// ViewContainers calls the provided function on a series of containers,
+	// stopping when done is true or err is non-nil, and returning err if it
+	// was non-nil.
+	ViewContainers(fn func(key uint64, c Container) (done bool, err error)) error
+	// ViewContainersRange is like ViewContainers, but covers only the containers
+	// matching the given range.
+	ViewContainersRange(first, last uint64, fn func(key uint64, c Container) (done bool, err error)) error
+}
+
+// WriteOnlyBitmap is an interface which lets us describe the composition of
+// ReadOnly and regular Bitmaps, and then do the same composition on
+// TransactionalBitmaps.
+type WriteOnlyBitmap interface {
 	// Add sets the given bit, reporting whether or not this was a change.
 	Add(uint64) bool
 	// Remove clears the given bit, reporting whether or not this was a change.
 	Remove(uint64) bool
-	// GetContainer yields a Container holding the 1<<16 values starting at
-	// key * (1<<16). For instance, if key is 1, it returns the bits from
-	// 65,536 to 131,071 inclusive.
-	GetContainer(key uint64) Container
 	// PutContainer overwrites the container at key with a new container.
 	PutContainer(key uint64, c Container)
-	// Union, etc, do what they say they would do. InPlace forms modify the given
-	// bitmap rather than creating a new one.
-	Union(...Bitmap) Bitmap
+	// UnionInPlace and related functions are parallel to the read-only
+	// forms, but may modify the underlying storage of the bitmap.
 	UnionInPlace(...Bitmap)
-	Intersection(...Bitmap) Bitmap
 	IntersectionInPlace(...Bitmap)
-	Difference(...Bitmap) Bitmap
 	DifferenceInPlace(...Bitmap)
-	Xor(...Bitmap) Bitmap
 	XorInPlace(...Bitmap)
-	// OffsetRange yields a new bitmap containing values from first to last,
-	// with all positions increased by offset. All three values must be multiples
-	// of 1<<16.
-	OffsetRange(offset, first, last uint64) Bitmap
 	// utility functions for updates
 	InvertRangeInPlace(first, last uint64)
 	// ImportRoaring, et al., handle importing bits from raw Roaring data.
 	// The Import case takes an additional parameter for whether or not the
 	// data is memory-mapped; if so, it should try to map container contents
-	// to that data if possible/applicable.
-	ImportRoaring(data []byte, mapped bool) error
+	// to that data if possible/applicable. Otherwise, and in the other cases,
+	// you should *not* use the provided data.
+	//
+	// ImportRoaring should indicate whether or not any containers are using
+	// the provided storage, so the caller can unmap if it's unused.
+	ImportRoaring(data []byte, mapped bool) (mappedAny bool, err error)
 	UnionInPlaceRoaring(data []byte) error
 	IntersectionInPlaceRoaring(data []byte) error
 	DifferenceInPlaceRoaring(data []byte) error
@@ -62,4 +84,54 @@ type Bitmap interface {
 	// It doesn't care whether the bits in the range are set, just whether the containers would contain them
 	// if they were.
 	ProcessContainersRange(first, last uint64, fn func(key uint64, c Container) (newC Container, write bool, done bool, err error)) error
+}
+
+// Bitmap represents an indexable vector of 1<<64 bits. Operations on a bitmap
+// are not guaranteed to be concurrency-safe. If you want concurrency guarantees,
+// you might need a TransactionalBitmap.
+type Bitmap interface {
+	ReadOnlyBitmap
+	WriteOnlyBitmap
+}
+
+// BitmapViewer is a function which operates on a read-only bitmap.
+type BitmapViewer func(b ReadOnlyBitmap) (err error)
+
+// BitmapUpdater is a function which operates on a bitmap. It should return
+// write=true if it wishes to apply its update, and a non-nil error if an
+// error occurred. If an error occurred, and write is true, the write should
+// still occur (but the error will be passed back up to another caller.)
+type BitmapUpdater func(b Bitmap) (write bool, err error)
+
+type TransactionalReadOnlyBitmap interface {
+	ReadOnlyBitmap
+	View(first, last uint64, fn BitmapViewer) error
+}
+
+// TransactionalBitmap supports transactions which can run concurrently and
+// safely.
+//
+// There exists an ordering on update operations for a given region of storage;
+// if operation A happens before operation B, then anything that happens before
+// operation A also happens before operation B.
+//
+// Any given view operation sees the state of the database after some set of
+// updates, and before some set of updates. (Either set might be empty.) An update
+// operation which starts after a view operation has begun executing will not
+// affect the data seen by the view.
+//
+// How this is accomplished is an implementation detail. For instance, you could
+// implement this with an RWMutex, with all views requesting a read lock, and
+// updates requesting a write lock. You could implement it with MVCC.
+//
+// ImmediateUpdate is a special case; it acts like Update, but is blocked by
+// and blocks View operations. For an implementation which would usually use
+// MVCC, this might omit a lot of overhead (such as copying), and allow improved
+// performance. However, it will likely degrade performance under a lot of
+// other workloads.
+type TransactionalBitmap interface {
+	TransactionalReadOnlyBitmap
+	WriteOnlyBitmap // lol
+	Update(first, last uint64, fn BitmapUpdater) error
+	ImmediateUpdate(first, last uint64, fn BitmapUpdater) error
 }
